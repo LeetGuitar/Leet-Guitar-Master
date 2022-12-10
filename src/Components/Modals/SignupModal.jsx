6d48c3f95498f7bb90be5d49153a1b465da11c97
Jacob <jakem417@icloud.com>
2022-12-10 13:06:31 -0700

    this should make login and signup modals grap input input values

--- src/Components/Modals/SignupModal.jsx
+++ src/Components/Modals/SignupModal.jsx
@@ -4,33 +4,20 @@ const SignupModal = (props) => {
   if (!props.showS) {
     return null;
   }
-  const signupSubmit = (e) => {
-    e.preventDefault();
-    //we are sending username and PW to the frontend as lowercase keys
-    const username = document.getElementById('signupUser').value;
-    const password = document.getElementById('signupPass').value;
-
-    const requestBody = { username, password };
-    if (username === '' || password === '') {
-      return alert('username or password cannot be blank');
-    }
-    console.log(requestBody);
-
-    e.target.reset();
-  };
+  
 
   return (
     <div className='signupbox'>
-      <form className='signup' onSubmit={signupSubmit}>
+      <form className='signup' onSubmit={props.onSubmit}>
         <h4>Sign up</h4>
         <label htmlFor='user'>Username</label>
         <br />
-        <input type='text' id='signupUser' id='inputbox' name='user' />
+        <input type='text' id='signupUser' className='inputbox' name='user' />
         <br />
 
         <label htmlFor='pass'>Password</label>
         <br />
-        <input type='text' id='signupPass' id='inputbox' name='pass'></input>
+        <input type='text' id='signupPass' className='inputbox' name='pass'></input>
         <br />
         <input type='submit' id='submit' name='submit'></input>
         <a href='#' onClick={props.onClose}>close</a>
