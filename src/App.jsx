d9402a7dabc8307453790551ac1e15ccf020fb22
Jacob <jakem417@icloud.com>
2022-12-10 08:53:27 -0700

    this will set up the html skeleton for the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Landing from './Components/Landing.jsx';
-import UserInfo from './Components/UserInfo.jsx';
+// import UserInfo from './Components/UserInfo.jsx';
 import Main from './Components/Main.jsx';
 import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/landing" element={<Landing />} />
-      <Route path="/" element={<Main />} />
-      <Route path="/user" element={<UserInfo />} />
+      <Route path="/" element={<Landing />} />
+      <Route path="/main" element={<Main />} />
+      {/* <Route path="/user" element={<UserInfo />} /> */}
     </Routes>
   );
 };
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -3,10 +3,30 @@ import LoginModal from './Modals/LoginModal.jsx';
 import SignupModal from './Modals/SignupModal.jsx';
 const Landing = () => {
   return (
-    <div className="landing">
-      <h2>This is the landing page</h2>
-      {/* <LoginModal />
-      <SignupModal /> */}
+    <div className='landing'>
+      <header className='logo'></header>
+      <div className='first-div'>
+        <img className='img'></img>
+        <div className='login-div'>
+          <h2>A New way to learn</h2>
+          <p>lorem ipsum</p>
+          <div className='btn-box'>
+            <button>Log In</button>
+            <button>Create an Account</button>
+          </div>
+        </div>
+      </div>
+      <div className='second-div'>
+        <div className='benefits'>
+          <h2>Take your guitar playing to the next level</h2>
+          <p>Technique</p>
+          <p>Theory</p>
+          <p>Songs</p>
+        </div>
+        <img className='img'></img>
+      </div>
+      <LoginModal />
+      <SignupModal />
     </div>
   );
 };
