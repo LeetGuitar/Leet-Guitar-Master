caff7fb9db8343ceffcaef0d42bcfa188fa93053
Andrew Hogan <andrewlallyhogan@gmail.com>
2022-12-12 10:57:42 -0500

    this will add the updated scales files

--- scales.js
+++ scales.js
@@ -1,13 +1,23 @@
 
+
+Ab, A, A# Bb B C C# Db D D# Eb E F F# Gb G G#
+
+INSERT INTO scales CSSFontPaletteValuesRule
 // ab
+
+'Ab Bb C Db Eb F G Ab', 'Ab Bb C D Eb F G Ab', 'Ab Bb C D Eb F Gb Ab', 'Ab Bb C Db Eb F Gb Ab', 'Ab Bb Cb Db Eb Fb Gb Ab', 'Ab Bbb Cb Db Eb Fb Gb Ab', 'Ab Bb Cb Db Eb F Gb Ab', 'Ab Bb Cb Db Eb Fb G Ab', 'Ab Bb Cb Db Eb F G Ab'
 
 const a_flat_major = 'Ab Bb C Db Eb F G Ab';
 
 const a_flat_lydian = 'Ab Bb C D Eb F G Ab';
 
+const a_flat_lydiandominant = 'Ab Bb C D Eb F Gb Ab';
+
 const a_flat_mixo = 'Ab Bb C Db Eb F Gb Ab';
 
 const a_flat_naturalminor = 'Ab Bb Cb Db Eb Fb Gb Ab';
+
+const a_flat_phrygian = 'Ab Bbb Cb Db Eb Fb Gb Ab';
 
 const a_flat_dorian = 'Ab Bb Cb Db Eb F Gb Ab';
 
@@ -17,13 +27,19 @@ const a_flat_melodicminor = 'Ab Bb Cb Db Eb F G Ab';
 
 // a
 
+'A B C# D E F# G# A', 'A B C# D# E F# G# A', 'A B C# D# E F# G A', 'A B C# D E F# G A', 'A B C D E F G A', 'A Bb C D E F G A', 'A B C D E F# G A', 'A B C D E F G# A', 'A B C D E F# G# A'
+
 const a_major = 'A B C# D E F# G# A'; // major
 
 const a_lydian = 'A B C# D# E F# G# A';
 
+const a_lydiandominant = 'A B C# D# E F# G A';
+
 const a_mixo = 'A B C# D E F# G A'; // major
 
 const a_naturalminor = 'A B C D E F G A';  // natural minor
+
+const a_phrygian = 'A Bb C D E F G A';
 
 const a_dorian = 'A B C D E F# G A';
 
@@ -33,13 +49,19 @@ const a_melodicminor = 'A B C D E F# G# A';
 
 // a#
 
+'A# B# Cx D# E# Fx Gx A#', 'A# B# Cx Dx E# Fx Gx A#', 'A# B# Cx Dx E# Fx G# A#', 'A# B# Cx D# E# Fx G# A#', 'A# B# C# D# E# F# G# A#', 'A# B C# D# E# F# G# A#', 'A# B# C# D# E# Fx G# A#', 'A# B# C# D# E# F# Gx A#', 'A# B# C# D# E# Fx Gx A#'
+
 const a_sharp_major = 'A# B# Cx D# E# Fx Gx A#';
 
 const a_sharp_lydian = 'A# B# Cx Dx E# Fx Gx A#';
 
+const a_sharp_lydiandominant = 'A# B# Cx Dx E# Fx G# A#';
+
 const a_sharp_mixo = 'A# B# Cx D# E# Fx G# A#';
 
 const a_sharp_naturalminor = 'A# B# C# D# E# F# G# A#';
+
+const a_sharp_phrygian = 'A# B C# D# E# F# G# A#';
 
 const a_sharp_dorian = 'A# B# C# D# E# Fx G# A#';
 
@@ -49,13 +71,19 @@ const a_sharp_melodicminor = 'A# B# C# D# E# Fx Gx A#';
 
 // bb
 
+'Bb C D Eb F G A Bb', 'Bb C D E F G A Bb', 'Bb C D E F G Ab Bb', 'Bb C D Eb F G Ab Bb', 'Bb C Db Eb F Gb Ab Bb', 'Bb Cb Db Eb F Gb Ab Bb', 'Bb C Db Eb F G Ab Bb', 'Bb C Db Eb F Gb A Bb', 'Bb C Db Eb F G A Bb'
+
 const b_flat_major = 'Bb C D Eb F G A Bb';
 
 const b_flat_lydian = 'Bb C D E F G A Bb';
 
+const b_flat_lydiandominant = 'Bb C D E F G Ab Bb';
+
 const b_flat_mixo = 'Bb C D Eb F G Ab Bb';
 
 const b_flat_naturalminor = 'Bb C Db Eb F Gb Ab Bb';
+
+const b_flat_phrygian = 'Bb Cb Db Eb F Gb Ab Bb';
 
 const b_flat_dorian = 'Bb C Db Eb F G Ab Bb';
 
@@ -65,13 +93,25 @@ const b_flat_melodicminor = 'Bb C Db Eb F G A Bb';
 
 // b
 
+'B C# D# E F# G# A# B', 'B C# D# E# F# G# A# B', 'B C# D# E# F# G# A B', 'B C# D# E F# G# A B', 'B C# D E F# G A B', 'B C D E F# G A B', 'B C# D E F# G# A B', 'B C# D E F# G A# B', 'B C# D E F# G# A# B'
+
+
+
+
+
+
+
 const b_major = 'B C# D# E F# G# A# B';
 
 const b_lydian = 'B C# D# E# F# G# A# B';
 
+const b_lydiandominant = 'B C# D# E# F# G# A B';
+
 const b_mixo = 'B C# D# E F# G# A B';
 
 const b_naturalminor = 'B C# D E F# G A B';
+
+const b_phrygian = 'B C D E F# G A B';
 
 const b_dorian = 'B C# D E F# G# A B';
 
@@ -81,13 +121,24 @@ const b_melodicminor = 'B C# D E F# G# A# B';
 
 // c
 
+'C D E F G A B C', 'C D E F# G A B C', 'C D E F# G A Bb C', 'C D E F G A Bb C', 'C D Eb F G Ab Bb C', 'C Db Eb F G Ab Bb C', 'C D Eb F G A Bb C', 'C D Eb F G Ab B C', 'C D Eb F G A B C'
+
+
+
+
+
+
 const c_major = 'C D E F G A B C';
 
 const c_lydian = 'C D E F# G A B C';
 
+const c_lydiandominant = 'C D E F# G A Bb C';
+
 const c_mixo = 'C D E F G A Bb C';
 
 const c_naturalminor = 'C D Eb F G Ab Bb C';
+
+const c_phrygian = 'C Db Eb F G Ab Bb C';
 
 const c_dorian = 'C D Eb F G A Bb C';
 
@@ -97,29 +148,50 @@ const c_melodicminor = 'C D Eb F G A B C';
 
 // c#
 
+'C# D# E# F# G# A# B# C#', 'C# D# E# Fx G# A# B# C#', 'C# D# E# Fx G# A# B C#', 'C# D# E# F# G# A# B C#', 'C# D# E F# G# A B C#', 'C# D E F# G# A B C#', 'C# D# E F# G# A# B C#', 'C# D# E F# G# A B# C#', 'C# D# E F# G# A# B C#'
+
+
+
+
+
 const c_sharp_major = 'C# D# E# F# G# A# B# C#';
 
 const c_sharp_lydian = 'C# D# E# Fx G# A# B# C#';
+
+const c_sharp_lydiandominant = 'C# D# E# Fx G# A# B C#';
 
 const c_sharp_mixo = 'C# D# E# F# G# A# B C#';
 
 const c_sharp_naturalminor = 'C# D# E F# G# A B C#';
 
+const c_sharp_phrygian = 'C# D E F# G# A B C#';
+
 const c_sharp_dorian = 'C# D# E F# G# A# B C#';
 
 const c_sharp_harmonicminor = 'C# D# E F# G# A B# C#';
 
-const c_sharp_melodicminor = 'C# D# E F# G# A# B C#';
+const c_sharp_melodicminor = 'C# D# E F# G# A# B# C#'; // <<<<<< c sharp melodic minor ERROR <<<<
 
 // db
+
+'Db Eb F Gb Ab Bb C Db', 'Db Eb F G Ab Bb C Db', 'Db Eb F G Ab Bb Cb Db', 'Db Eb F Gb Ab Bb Cb Db', 'Db Eb Fb Gb Ab Bbb Cb Db', 'Db Ebb Fb Gb Ab Bbb Cb Db', 'Db Eb Fb Gb Ab Bb Cb Db', 'Db Eb Fb Gb Ab Bbb C Db', 'Db Eb Fb Gb Ab Bb C Db'
+
+
+
+
+
 
 const d_flat_major = 'Db Eb F Gb Ab Bb C Db';
 
 const d_flat_lydian = 'Db Eb F G Ab Bb C Db';
 
+const d_flat_lydiandominant = 'Db Eb F G Ab Bb Cb Db';
+
 const d_flat_mixo = 'Db Eb F Gb Ab Bb Cb Db';
 
 const d_flat_naturalminor = 'Db Eb Fb Gb Ab Bbb Cb Db';
+
+const d_flat_phrygian = 'Db Ebb Fb Gb Ab Bbb Cb Db';
 
 const d_flat_dorian = 'Db Eb Fb Gb Ab Bb Cb Db';
 
@@ -129,13 +201,24 @@ const d_flat_melodicminor = 'Db Eb Fb Gb Ab Bb C Db';
 
 // d
 
+'D E F# G A B C# D', 'D E F# G# A B C# D', 'D E F# G# A B C D', 'D E F# G A B C D', 'D E F G A Bb C D', 'D Eb F G A Bb C D', 'D E F G A B C D', 'D E F G A Bb C# D', 'D E F G A B C# D'
+
+
+
+
+
+
 const d_major = 'D E F# G A B C# D';
 
 const d_lydian = 'D E F# G# A B C# D';
 
+const d_lydiandominant = 'D E F# G# A B C D';
+
 const d_mixo = 'D E F# G A B C D';
 
 const d_naturalminor = 'D E F G A Bb C D';
+
+const d_phrygian = 'D Eb F G A Bb C D';
 
 const d_dorian = 'D E F G A B C D';
 
@@ -145,13 +228,25 @@ const d_melodicminor = 'D E F G A B C# D';
 
 // d#
 
+'D# E# Fx G# A# B# Cx D#', 'D# E# Fx Gx A# B# Cx D#', 'D# E# Fx Gx A# B# C# D#', 'D# E# Fx G# A# B# C# D#', 'D# E# F# G# A# B C# D#', 'D# E F# G# A# B C# D#', 'D# E# F# G# A# B# C# D#', 'D# E# F# G# A# B Cx D#', 'D# E# F# G# A# B# Cx D#'
+
+
+
+
+
+
+
 const d_sharp_major = 'D# E# Fx G# A# B# Cx D#';
 
 const d_sharp_lydian = 'D# E# Fx Gx A# B# Cx D#';
 
+const d_sharp_lydiandominant = 'D# E# Fx Gx A# B# C# D#';
+
 const d_sharp_mixo = 'D# E# Fx G# A# B# C# D#';
 
 const d_sharp_naturalminor = 'D# E# F# G# A# B C# D#';
+
+const d_sharp_phrygian = 'D# E F# G# A# B C# D#';
 
 const d_sharp_dorian = 'D# E# F# G# A# B# C# D#';
 
@@ -161,13 +256,25 @@ const d_sharp_melodicminor = 'D# E# F# G# A# B# Cx D#';
 
 // eb
 
+'Eb F G Ab Bb C D Eb', 'Eb F G A Bb C D Eb', 'Eb F G A Bb C Db Eb', 'Eb F G Ab Bb C Db Eb', 'Eb F Gb Ab Bb Cb Db Eb', 'Eb Fb Gb Ab Bb Cb Db Eb', 'Eb F Gb Ab Bb C Db Eb', 'Eb F Gb Ab Bb Cb D Eb', 'Eb F Gb Ab Bb C D Eb'
+
+
+
+
+
+
+
 const e_flat_major = 'Eb F G Ab Bb C D Eb';
 
 const e_flat_lydian = 'Eb F G A Bb C D Eb';
 
+const e_flat_lydiandominant = 'Eb F G A Bb C Db Eb';
+
 const e_flat_mixo = 'Eb F G Ab Bb C Db Eb';
 
 const e_flat_naturalminor = 'Eb F Gb Ab Bb Cb Db Eb';
+
+const e_flat_phrygian = 'Eb Fb Gb Ab Bb Cb Db Eb';
 
 const e_flat_dorian = 'Eb F Gb Ab Bb C Db Eb';
 
@@ -177,13 +284,23 @@ const e_flat_melodicminor = 'Eb F Gb Ab Bb C D Eb';
 
 // e
 
+
+'E F# G# A B C# D# E', 'E F# G# A# B C# D# E', 'E F# G# A# B C# D E', 'E F# G# A B C# D E', 'E F# G A B C D E', 'E F G A B C D E', 'E F# G A B C# D E', 'E F# G A B C D# E', 'E F# G A B C# D# E'
+
+
+
+
 const e_major = 'E F# G# A B C# D# E';
 
 const e_lydian = 'E F# G# A# B C# D# E';
 
+const e_lydiandominant = 'E F# G# A# B C# D E';
+
 const e_mixo = 'E F# G# A B C# D E';
 
 const e_naturalminor = 'E F# G A B C D E';
+
+const e_phrygian = 'E F G A B C D E';
 
 const e_dorian = 'E F# G A B C# D E';
 
@@ -193,13 +310,21 @@ const e_melodicminor = 'E F# G A B C# D# E';
 
 // f
 
+
+'F G A Bb C D E F', 'F G A B C D E F', 'F G A B C D Eb F', 'F G A Bb C D Eb F', 'F G Ab Bb C Db Eb F', 'F Gb Ab Bb C Db Eb F', 'F G Ab Bb C D Eb F', 'F G Ab Bb C Db E F', 'F G Ab Bb C D E F'
+
+
 const f_major = 'F G A Bb C D E F';
 
 const f_lydian = 'F G A B C D E F';
 
+const f_lydiandominant = 'F G A B C D Eb F';
+
 const f_mixo = 'F G A Bb C D Eb F';
 
 const f_naturalminor = 'F G Ab Bb C Db Eb F';
+
+const f_phrygian = 'F Gb Ab Bb C Db Eb F';
 
 const f_dorian = 'F G Ab Bb C D Eb F';
 
@@ -209,13 +334,25 @@ const f_melodicminor = 'F G Ab Bb C D E F';
 
 // f#
 
+
+'F# G# A# B C# D# E# F#', 'F# G# A# B# C# D# E# F#', 'F# G# A# B# C# D# E F#', 'F# G# A# B C# D# E F#', 'F# G# A B C# D E F#', 'F# G A B C# D E F#', 'F# G# A B C# D# E F#', 'F# G# A B C# D E# F#', 'F# G# A B C# D# E# F#'
+
+
+
+
+
+
 const f_sharp_major = 'F# G# A# B C# D# E# F#';
 
 const f_sharp_lydian = 'F# G# A# B# C# D# E# F#';
 
+const f_sharp_lydiandominant = 'F# G# A# B# C# D# E F#';
+
 const f_sharp_mixo = 'F# G# A# B C# D# E F#';
 
 const f_sharp_naturalminor = 'F# G# A B C# D E F#';
+
+const f_sharp_phrygian = 'F# G A B C# D E F#';
 
 const f_sharp_dorian = 'F# G# A B C# D# E F#';
 
@@ -225,13 +362,21 @@ const f_sharp_melodicminor = 'F# G# A B C# D# E# F#';
 
 // gb
 
+
+'Gb Ab Bb Cb Db Eb F Gb', 'Gb Ab Bb C Db Eb F Gb', 'Gb Ab Bb C Db Eb Fb Gb', 'Gb Ab Bb Cb Db Eb Fb Gb', 'Gb Ab Bbb Cb Db Ebb Fb Gb', 'Gb Abb Bbb Cb Db Ebb Fb Gb', 'Gb Ab Bbb Cb Db Eb Fb Gb', 'Gb Ab Bbb Cb Db Ebb F Gb', 'Gb Ab Bbb Cb Db Eb F Gb'
+
+
 const g_flat_major = 'Gb Ab Bb Cb Db Eb F Gb';
 
 const g_flat_lydian = 'Gb Ab Bb C Db Eb F Gb';
 
+const g_flat_lydiandominant = 'Gb Ab Bb C Db Eb Fb Gb';
+
 const g_flat_mixo = 'Gb Ab Bb Cb Db Eb Fb Gb';
 
 const g_flat_naturalminor = 'Gb Ab Bbb Cb Db Ebb Fb Gb';
+
+const g_flat_phrygian = 'Gb Abb Bbb Cb Db Ebb Fb Gb';
 
 const g_flat_dorian = 'Gb Ab Bbb Cb Db Eb Fb Gb';
 
@@ -241,13 +386,19 @@ const g_flat_melodicminor = 'Gb Ab Bbb Cb Db Eb F Gb';
 
 // g
 
+'G A B C D E F# G', 'G A B C# D E F# G', 'G A B C# D E F G', 'G A B C D E F G', 'G A Bb C D Eb F G', 'G Ab Bb C D Eb F G', 'G A Bb C D E F G', 'G A Bb C D Eb F# G', 'G A Bb C D E F# G'
+
 const g_major = 'G A B C D E F# G';
 
 const g_lydian = 'G A B C# D E F# G';
 
+const g_lydiandominant = 'G A B C# D E F G';
+
 const g_mixo = 'G A B C D E F G';
 
 const g_naturalminor = 'G A Bb C D Eb F G';
+
+const g_phrygian = 'G Ab Bb C D Eb F G';
 
 const g_dorian = 'G A Bb C D E F G';
 
@@ -257,16 +408,28 @@ const g_melodicminor = 'G A Bb C D E F# G';
 
 // g#
 
+
+'G# A# B# C# D# E# Fx G#', 'G# A# B# Cx D# E# Fx G#', 'G# A# B# Cx D# E# F# G#', 'G# A# B# C# D# E# F# G#', 'G# A# B C# D# E F# G#', 'G# A B C# D# E F# G#', 'G# A# B C# D# E# F# G#', 'G# A# B C# D# E Fx G#', 'G# A# B C# D# E# Fx G#'
+
+
+
+
+
+
 const g_sharp_major = 'G# A# B# C# D# E# Fx G#';
 
 const g_sharp_lydian = 'G# A# B# Cx D# E# Fx G#';
+
+const g_sharp_lydiandominant = 'G# A# B# Cx D# E# F# G#';
 
 const g_sharp_mixo = 'G# A# B# C# D# E# F# G#';
 
 const g_sharp_naturalminor = 'G# A# B C# D# E F# G#';
 
+const g_sharp_phrygian = 'G# A B C# D# E F# G#';
+
 const g_sharp_dorian = 'G# A# B C# D# E# F# G#';
 
-const g_sharp_harmonicminor = 'G# A# B C# D# E F# G#';
+const g_sharp_harmonicminor = 'G# A# B C# D# E Fx G#';
 
-const g_sharp_melodicminor = 'G# A# B C# D# E# F# G#';
+const g_sharp_melodicminor = 'G# A# B C# D# E# Fx G#';
